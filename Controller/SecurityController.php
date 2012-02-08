4ea551ec968516bac09153229c5847c1a83c2885
Ryan Rogers <webmaster@epicoftimewasted.com>
2012-02-08 12:19:53 -0800

    Start laying the groundwork for login throttling

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -12,6 +12,15 @@ class SecurityController extends Controller
 	{
 		$request = $this->get('request');
 		$session = $request->getSession();
+		/**
+		 * FIXME: I'm not sure this is the best way to handle null sessions.
+		 */
+/*
+		if( $session === null ) {
+			$session = $this->get('session');
+			$request->setSession($session);
+		}
+*/
 
 		/**
 		 * Check to see if there is an authentication error.
@@ -26,17 +35,60 @@ class SecurityController extends Controller
 		}
 
 		/**
-		 * If there was an authentication error, return a generic error message
-		 * to avoid leaking anything potentially sensitive.  Also, check to see
-		 * if there is a referer URL saved to the session, so that we can send
-		 * the user back to the correct page even after a failed auth attempt.
+		 * Check to see if this login requires a captcha, and if so, if the
+		 * submitted captcha is valid.
+		 *
+		 * FIXME: This is part of the not yet implemented login throttling.
+		 */
+/*
+		if( $session->has('_security.login_requires_captcha') ) {
+			if( $this->get('epicoftimewasted_user.captcha')->isCaptchaValid() !== true )
+				$error = true;
+			$session->remove('_security.login_requires_captcha');
+		}
+*/
+
+		/**
+		 * Perform error handling tasks.
 		 */
 		if( $error !== null ) {
+			/**
+			 * First, replace the error message with something generic to
+			 * avoid potentially leaking sensitive information.
+			 */
 			$error = 'security.login.authentication_failed';
+
+			/**
+			 * Reload the referer URL so that we can send the user back to the
+			 * correct page even after a failed authorization attempt.
+			 */
 			if( $session->has('_security.login_referer_url') ) {
 				$referer = $session->get('_security.login_referer_url');
 				$session->remove('_security.login_referer_url');
 			}
+
+			/**
+			 * Perform login rate throttling, if needed.
+			 *
+			 * FIXME: Login throttling is not yet implemented.
+			 */
+/*
+			if( $this->get('epicoftimewasted_user.login_throttling.enabled') === true ) {
+				$userManager = $this->get('epicoftimewasted_user.user_manager');
+				$user = $userManager->findUserByUsername($session->get(SecurityContext::LAST_USERNAME));
+				if( $user !== null ) {
+					$threshold = $this->get('epicoftimewasted_user.login_throttling.threshold');
+					if( $user->getFailedLoginAttempts() > $threshold ) {
+						if( $this->get('epicoftimewasted_user.captcha.enabled') === true ) {
+							$captcha = $this->get('epicoftimewasted_user.captcha')->generateCaptcha();
+							$session->set('_security.login_requires_captcha', true);
+						} else {
+							// Implement a timed delay.
+						}
+					}
+				}
+			}
+*/
 		}
 
 		/**
@@ -75,8 +127,10 @@ class SecurityController extends Controller
 			'error' => $error,
 			'referer' => empty($referer) ? null : $referer,
 			'last_username' => null,
+//			'captcha' => isset($captcha) ? $captcha : null,
 		));
 		$response->setPrivate();
+		$response->setMaxAge(0);
 		return $response;
 	}
 
